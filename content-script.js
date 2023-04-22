e59c3833c7d87bc2990e9f58c87ad0c0cfd5e49d
김현철 <narx17@gmail.com>
2023-04-22 23:02:09 +0900

    guard

--- content-script.js
+++ content-script.js
@@ -2,6 +2,7 @@ const resultElement = document.createElement("div");
 chrome.storage.sync.get("options", ({options}) => {
     if (!options) {
         alert("Please set your options in the extension settings. from better comment extension");
+        return
     }
     const analyzeComment = async (comment) => {
         resultElement.innerHTML = `<marquee>분석중...제발 말 좀 곱게 합시다...</marquee>`;
